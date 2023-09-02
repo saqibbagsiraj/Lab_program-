34c6c9e8c18671ef3125f5d668f82aad37cdd635
saqibbagsiraj <139454271+saqibbagsiraj@users.noreply.github.com>
2023-09-02 12:26:35 +0530

    Create Lab3.py

---
+++ Lab3.py
@@ -0,0 +1,23 @@
+#Mean,Variance and standard deviation 
+ n = int(input("Enter the number students: ")) 
+ list1 = [] 
+ for i in range(n): 
+ marks = int(input("Enter marks of each student in python: ")) 
+ list1.append(marks) 
+ print("List containing marks of student: ", list1) 
+ print("-"*50) 
+ # Finding Mean 
+ sum = 0 # Initialize 
+ for i in list1: 
+ sum += i 
+ print("Sum of all the elements in the list: ", sum) 
+ x_bar = sum/len(list1) 
+ print("Mean: ", x_bar) 
+ # Finding Variance 
+ var = 0 # Initialize 
+ for i in list1: 
+ var = var + ((i - x_bar)**2)/len(list1) 
+ print("Variance: ", var) 
+ # Finding Standard Deviation 
+ std_dev = var**0.5 
+ print("Standard Deviation: ", std_dev)
